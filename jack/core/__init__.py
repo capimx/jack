89f1f8573a912208f707bd8ee8057eae722e3a8a
Takuma Yoneda <takuma-ynd@users.noreply.github.com>
2018-07-05 12:09:27 +0100

    add debugging feature

--- jack/core/__init__.py
+++ jack/core/__init__.py
@@ -4,3 +4,4 @@ from jack.core.output_module import *
 from jack.core.reader import *
 from jack.core.tensorport import *
 from jack.core.shared_resources import *
+from jack.core.debugging import *
---
+++ jack/core/debugging.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+
+class HyperParams:
+    class OutputLayer:
+        bias = np.array([-1., -1., 0.])
